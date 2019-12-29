8a5b544d1bc0cd39e2210f019a9109ae31ba5efb
wangkeke <wangkeke565@163.com>
2019-12-29 11:14:49 +0800

    spring security http login test url : /static/login.html ;

--- com.swarm.web/src/main/java/com/swarm/web/CustomWebSecurityConfig.java
+++ com.swarm.web/src/main/java/com/swarm/web/CustomWebSecurityConfig.java
@@ -16,7 +16,7 @@ public class CustomWebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 		.authorizeRequests()
-			.antMatchers("/login/captcha").permitAll()
+			.antMatchers("/favicon.ico","/login/captcha","/actuator","/actuator/**","/static/**").permitAll()
 			.anyRequest().fullyAuthenticated()
 //			.hasAnyAuthority(Identity.ADMIN_ID.getName(),
 //			Identity.SYSTEM_ID.getName(),Identity.USER_ID.getName())
